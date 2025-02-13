41b8bda93f61444befb35268b4f6157ae4b009de
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-02-13 21:47:47 +0530

    🎨 design(ui): set up initial UI layout and structured essential pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,160 @@
 import Image from "next/image";
+import "./style.css";
+import Avvvatars from "avvvatars-react";
+// import { DribbbleIcon } from "@/components/ui/dribbble";
+// import { TwitchIcon } from "@/components/ui/twitch";
+// import { LinkedinIcon } from "@/components/ui/linkedin";
+// import { YoutubeIcon } from "@/components/ui/youtube";
+import { InstagramIcon } from "@/components/ui/instagram";
+import {
+  ArrowRight,
+  Github,
+  LucideGithub,
+  MailPlus,
+  ShieldCheckIcon,
+} from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="container">
+        <div className="nav">
+          <div className="leftC">
+            <div className="logo">
+              {" "}
+              <Avvvatars
+                style="shape"
+                value="hello 32link124567153fgdfy35ey"
+                size={60}
+              />
+            </div>
+            <div className="linkContainer">
+              <div className="link">Home</div>
+              <div className="link">Create</div>
+              <div className="link">Dashboard</div>
+              <div className="link">Contribute</div>
+            </div>
+          </div>
+          <div className="rightC">
+            {" "}
+            <div className="buttonContainer">
+              <div className="button">Try it out</div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="home">
+          <div className="textBig">hellolink</div>
+          <div className="textSmall">
+            Tired of messy bios? Say hello to a single, powerful link that
+            organizes and shares everything you love in one place.{" "}
+          </div>
+          <a href="#signup"><div className="button">Explore all</div></a>
+        </div>
+        <div className="feature">
+          <div className="box">
+            <div className="top">
+              <div className="code1">hellolink/username</div>
+            </div>
+            <div className="bottom">
+              Get a short, shareable link for a clean online identity.
+            </div>
+          </div>
+          <div className="box">
+            {" "}
+            <div className="top">
+              <div className="containerFull">
+                <div className="code">
+                  <div className="line"></div>
+                </div>
+                <div className="code">
+                  <div className="line"></div>
+                </div>
+                <div className="code">
+                  <div className="line"></div>
+                </div>
+              </div>
+            </div>
+            <div className="bottom">
+              One sleek layout that adapts beautifully to all users.
+            </div>
+          </div>
+          <div className="box">
+            {" "}
+            <div className="top">
+              <div className="containerFull">
+                <div className="instagramSquare">
+                  <div className="icon">
+                    <InstagramIcon />
+                  </div>
+                  <div className="text1"></div>
+                  <div className="text2"></div>
+                </div>
+              </div>
+            </div>
+            <div className="bottom">
+              Add instant actions for WhatsApp, email, or DMs.
+            </div>
+          </div>
+        </div>
+<div id="signup"></div>
+        <div className="signup">
+          <div className="left">
+            <div className="box">
+              <div className="textTop">Sign up and share!</div>
+              <div className="textBottom">
+                Join HelloLink and create your personalized link page in
+                seconds. Share all your important links with a single, sleek
+                profile. Sign up now—it's quick, easy, and free!
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <div className="box">
+              <div className="header">
+                <div className="pfp"></div>
+                <div className="text">hellolink/divyanshudhruv</div>
+              </div>
+              <input type="text" placeholder="username" id="username" />
+              <input type="email" placeholder="e-mail" id="e-mail" />
+              <div className="buttonSignUp">
+                Sign up&nbsp;&nbsp;
+                <ArrowRight size={23} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer">
+          <div className="left">
+            <div className="top">
+              <div className="textTop">hellolink</div>
+              <div className="buttonC">
+              <a href="#signup"><div className="button1">Try now</div></a>
+                <div className="button2">
+                  {" "}
+                  <a
+                    href="https://github.com/divyanshudhruv/hellolink"
+                    target="_blank"
+                  >
+                    Contribute
+                  </a>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          <div className="right">
+            <div className="box">© 2025 HelloLink. All rights reserved.</div>
+          </div>
+        </div>
+        <div className="lastLine">
+          made with bugs by{" "}
+          <u>
+            <a href="https://github.com/divyanshudhruv" target="_blank">
+              @divyanshudhruv
+            </a>
+          </u>
+        </div>
+      </div>
+    </>
   );
 }
