8de70923327a1f325688442cf4a0337ae9306d7e
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2025-02-15 02:55:08 +0530

    ⚡ perf(hydration): optimize layout rendering to prevent hydration mismatch

---
+++ app/[username]/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}): Promise<Metadata> {
+  return {
+    title: `@${params.username} - hellolink`,
+    description: "👋 Hello to your ⛓️‍💥 links, all in one 🏠 place.",
+  };
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/create/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}): Promise<Metadata> {
+  return {
+    title: `create - hellolink`,
+    description: "👋 Hello to your ⛓️‍💥 links, all in one 🏠 place.",
+  };
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
